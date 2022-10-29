5b2c8963b4878e9d73e6e8f718a031ecd3db6a54
Hasan Mohamed Assem <hasan.assem99@gmail.com>
2022-10-30 01:23:10 +0200

    proj created

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,12 @@
+import "./Content.css"
+
+
+const Content = () => {
+    return(
+        <div className="content--container">
+            <h1>Content</h1>
+        </div>
+    )
+}
+
+export default Content
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+const Header =() =>{
+    return(
+        <header>
+            <h1 className="mt-4 p5 bg-primary text-white rounded">header</h1>
+        </header>
+    )
+}
+
+export default Header
